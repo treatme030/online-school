8df24c05a63426c40efa05ebe12b7cc4cec1aa57
treatme030 <treatme8192@gmail.com>
2022-08-19 23:12:04 +0900

    add CssBaseline

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material';
 import theme from './theme';
 import App from './App';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App />
     </ThemeProvider>
   </React.StrictMode>
