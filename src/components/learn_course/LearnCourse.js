b1c8c7658ae6f19617ea5e4f5df1c416f64e0e32
treatme030 <treatme8192@gmail.com>
2022-08-29 23:01:03 +0900

    add learn course component

---
+++ src/components/learn_course/LearnCourse.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Box, List, ListItem, ListItemText, Typography } from '@mui/material';
+
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import styles from './styles';
+
+const LearnCourse = () => {
+  return (
+    <Box sx={styles.wrapper}>
+      <Typography variant='h3' sx={styles.title}>
+        What you'll learn in this course:
+      </Typography>
+      <List>
+        {[1, 2, 3, 4, 5].map((_, index) => (
+          <ListItem key={index} sx={styles.listItem}>
+            <FiberManualRecordIcon sx={styles.icon} />
+            <Typography sx={styles.listItemText}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            </Typography>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+};
+
+export default LearnCourse;
---
+++ src/components/learn_course/styles.js
@@ -0,0 +1,25 @@
+const styles = {
+  wrapper: {
+    mt: '48px',
+  },
+  title: (theme) => ({
+    fontWeight: 500,
+    color: theme.palette.typography.title,
+    mb: '10px',
+  }),
+  listItem: {
+    pl: '0px',
+  },
+  icon: (theme) => ({
+    color: theme.palette.primary.dark,
+    width: '10px',
+  }),
+  listItemText: (theme) => ({
+    color: theme.palette.typography.desc,
+    fontSize: '14px',
+    lineHeight: '17px',
+    ml: '10px',
+  }),
+};
+
+export default styles;
